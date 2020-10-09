e7ad8a8291218c50392a0eaa65f7e3605736d25a
Sailesh Pandey <saileshpandey007007@gmail.com>
2020-10-09 15:33:37 +0545

    Tic-Tac-Toe mini responsive game

---
+++ tictaktoe.cpp
@@ -0,0 +1,198 @@
+#include <iostream>
+using namespace std;
+char matrix[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char player = 'X';
+int count;
+void Plot()
+{
+	system("cls"); //he system() function is a part of the C/C++ standard library. 
+    //It is used to pass the commands that can be executed in the command processor or the terminal of the operating system, 
+    //and finally returns the command after it has been completed.
+	cout << "Tic Tac Toe v1.0" << endl;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+void Input()
+{
+	int a;
+	cout << "It's " << player << " turn. " <<"Press the number of the field: ";
+	cin >> a;
+
+	if (a == 1)
+	{
+		if (matrix[0][0] == '1')
+			matrix[0][0] = player;
+		else
+		{
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	else if (a == 2)
+	{
+		if (matrix[0][1] == '2')
+			matrix[0][1] = player;
+		else
+		{
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	else if (a == 3)
+	{
+		if (matrix[0][2] == '3')
+			matrix[0][2] = player;
+		else
+		{
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	else if (a == 4)
+	{
+		if (matrix[1][0] == '4')
+			matrix[1][0] = player;
+		else
+		{
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	else if (a == 5)
+	{
+		if (matrix[1][1] == '5')
+			matrix[1][1] = player;
+		else
+		{
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	else if (a == 6)
+	{
+		if (matrix[1][2] == '6')
+			matrix[1][2] = player;
+		else
+		{
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	else if (a == 7)
+	{
+		if (matrix[2][0] == '7')
+			matrix[2][0] = player;
+		else
+		{
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	else if (a == 8)
+	{
+		if (matrix[2][1] == '8')
+			matrix[2][1] = player;
+		else
+		{
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	else if (a == 9)
+	{
+		if (matrix[2][2] == '9')
+			matrix[2][2] = player;
+		else
+		{
+			cout << "Field is already in use try again!" << endl;
+			Input();
+		}
+	}
+	
+}
+void playerChange()
+{
+	if (player == 'X')
+		player = 'O';
+	else
+		player = 'X';
+}
+char Win()
+{
+	//first player
+	if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X')
+		return 'X';
+	if (matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X')
+		return 'X';
+	if (matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X')
+		return 'X';
+
+	if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X')
+		return 'X';
+	if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X')
+		return 'X';
+	if (matrix[0][2] == 'X' && matrix[1][2] == 'X' && matrix[2][2] == 'X')
+		return 'X';
+
+	if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X')
+		return 'X';
+	if (matrix[2][0] == 'X' && matrix[1][1] == 'X' && matrix[0][2] == 'X')
+		return 'X';
+
+	//second player
+	if (matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O')
+		return 'O';
+	if (matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O')
+		return 'O';
+	if (matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O')
+		return 'O';
+
+	if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O')
+		return 'O';
+	if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O')
+		return 'O';
+	if (matrix[0][2] == 'O' && matrix[1][2] == 'O' && matrix[2][2] == 'O')
+		return 'O';
+
+	if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O')
+		return 'O';
+	if (matrix[2][0] == 'O' && matrix[1][1] == 'O' && matrix[0][2] == 'O')
+		return 'O';
+
+	return '/';
+}
+int main()
+{
+	count = 0;
+	Plot();
+	while (1) 
+	{
+		count++;
+		Input();
+		Plot();
+		if (Win() == 'X')
+		{
+			cout << "X won!" << endl;
+			break;
+		}
+		else if (Win() == 'O')
+		{
+			cout << "O won!" << endl;
+			break;
+		}
+		else if (Win() == '/' && count == 9)
+		{
+			cout << "Game Draw!" << endl;
+			break;
+		}
+		playerChange();
+	}
+	system("pause");
+	return 0;
+}
